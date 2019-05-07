306a8606453e2bd9e9c4a2b7e0c943be06b99db0
drthanhtikeaung <aung@aung.me>
2019-05-07 22:18:05 +0630

    init decrypApp.

---
+++ DecryptApp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DecryptApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOpenPublickeyFile_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.DefaultExt = "*.*";
+            dialog.Filter = "All Files|*.*";
+            dialog.Multiselect = false;
+
+            if (dialog.ShowDialog() == true)
+            {
+                
+            }
+        }
+
+        private void BtnDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
